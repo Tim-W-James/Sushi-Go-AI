f102fff28b419c120d6957cf4080e0304612391e
timja <u6947396@anu.edu.au>
2019-05-25 20:47:56 +1000

    Main Task - greedyCard, buildGSTree Defined

--- src/AI.hs
+++ src/AI.hs
@@ -13,11 +13,36 @@ type AIFunc
   -> Int -- ^ How far you should look ahead
   -> Move
 
+data GSTree a = GSTree a [GSTree a]
+
+instance Show a => Show (GSTree a) where
+    show tree = pretty tree
+        where
+          pretty :: Show a => GSTree a -> String
+          pretty = unlines.layout
+          layout :: Show a => GSTree a -> [String]
+          layout (GSTree v []) = [show v]
+          layout (GSTree v children) = [show v] ++ concat (map indent (map layout children))
+          indent :: [String] -> [String]
+          indent = map ("  "++)
+
+largeGameState :: GameState
+largeGameState = initialGame [(Nigiri 1),(Nigiri 1),(Wasabi Nothing),Dumplings,Eel,Tofu,Sashimi] [(Nigiri 2),(Nigiri 2),(Wasabi Nothing),Dumplings,Eel,Tofu,Sashimi]
+
+mediumGameState :: GameState
+mediumGameState = initialGame [(Nigiri 1),Dumplings,Tofu] [(Nigiri 2),Eel,Sashimi]
+
+smallGameState :: GameState
+smallGameState = initialGame [(Nigiri 1),Dumplings] [(Nigiri 2),Eel]
+
+tinyGameState :: GameState
+tinyGameState = initialGame [Dumplings] [Eel]
+
 -- | The table of all AIs you have implemented. We will mark the AI
 -- called "default" as your submission, but you may include other AIs
 -- for testing.
 ais :: [(String, AIFunc)]
-ais = [("default", firstCard)]
+ais = [("firstLegal", firstCard),("greedy", greedyCard)]
 
 -- Equivalently: firstLegal :: GameState -> Int -> Move
 -- firstLegal simply takes the first card it sees
@@ -25,3 +50,43 @@ firstCard :: AIFunc
 firstCard state _ = case gameStatus state of
   Turn player -> TakeCard (head (handFor player state))
   _ -> error "firstCard: called on finished game"
+
+greedyCard :: AIFunc
+greedyCard state _ = case gameStatus state of
+  Turn player -> TakeCard (greedyGreatest (greedyHeuristic (handFor player state) (cardsFor player state) []))
+  _ -> error "greedyCard: called on finished game"
+
+greedyHeuristic  :: [Card] -> [Card] -> [(Card,Int)] -> [(Card,Int)]
+greedyHeuristic hand chosenCards carry = case hand of
+  [] -> carry
+  x:xs -> greedyHeuristic xs chosenCards ((x,(scoreCards (x : chosenCards))) : carry)
+
+greedyGreatest :: [(Card,Int)] -> Card
+greedyGreatest handOptions = case handOptions of
+  [] -> error "greedyGreatest: hand is empty"
+  x:xs -> greedyGreatestHelper xs x
+
+  where
+    greedyGreatestHelper :: [(Card,Int)] -> (Card,Int) -> Card
+    greedyGreatestHelper options (currCard,currScore) = case options of
+      [] -> currCard
+      (card,score):xs
+        | score > currScore -> greedyGreatestHelper xs (card,score)
+        | otherwise -> greedyGreatestHelper xs (currCard,currScore)
+
+gsMoves :: GameState -> [GameState]
+gsMoves gameState@(GameState s p1h _ p2h _) = case s of
+  Turn Player1 -> gsMovesHelper gameState p1h []
+  Turn Player2 -> gsMovesHelper gameState p2h []
+  _ -> [gameState]
+
+  where
+    gsMovesHelper :: GameState -> [Card] -> [GameState] -> [GameState]
+    gsMovesHelper gs remHand carry = case remHand of
+      [] -> carry
+      x:xs -> gsMovesHelper gs xs (playMove (TakeCard x) gs : carry)
+
+buildGSTree :: GameState -> GSTree GameState
+buildGSTree gs = case gameStatus gs of
+  Finished -> GSTree gs []
+  _ -> GSTree gs (map buildGSTree (gsMoves gs))
